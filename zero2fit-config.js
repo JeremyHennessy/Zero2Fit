4c6138f45713e3b429fe8bd8055e7d2bafcfda03
Jeremy Hennessy <jeremy.hennessy@gmail.com>
2026-09-03 11:16:29 -0300

    Build 035: load auth activation blocker fix

--- zero2fit-config.js
+++ zero2fit-config.js
@@ -2,3 +2,5 @@ window.ZERO2FIT_CONFIG = Object.freeze({
   supabaseUrl: 'https://guxdnxnqzhkidtastsfb.supabase.co',
   supabasePublishableKey: 'sb_publishable_pRhqHz4vEl0plyGuXx8Ztg_JfSxg57m'
 });
+
+import('./build035-auth-activation.js').catch(error => console.warn('Zero2Fit Build 035 auth activation failed to load', error));
